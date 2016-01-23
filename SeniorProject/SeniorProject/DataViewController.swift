ca84410625a7cad5bc4e8fab5a6c85fb679efe2c
Aaron Rosenberger <RosenbergerGA1@gcc.edu>
2016-01-22 20:06:21 -0500

    Added blank project with Maps permissions

---
+++ SeniorProject/SeniorProject/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  SeniorProject
+//
+//  Created by Aaron Rosenberger on 1/21/16.
+//  Copyright © 2016 Aaron Rosenberger. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
