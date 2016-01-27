318e19a81c2807c29f61121d21eb137769426e13
mkytkajr65 <mkytkajr@gmail.com>
2016-01-26 22:13:46 -0500

    fixed conflict

--- Parse/cloud/main.js
+++ Parse/cloud/main.js
@@ -1,10 +1,7 @@
 require('cloud/app.js');
 require('cloud/test.js');
-<<<<<<< HEAD
 require('cloud/createUser.js');
-=======
 require('cloud/login.js');
->>>>>>> origin/master
 
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
