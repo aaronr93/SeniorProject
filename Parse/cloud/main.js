3bc0cb754327e9aa040d7dff9062965e3e74dc04
Zach Nafziger <zachary@nafziger.net>
2016-02-15 16:19:52 -0500

    cloud code for my orders
    delete order, get my sent requests, get requests i'm picking up

--- Parse/cloud/main.js
+++ Parse/cloud/main.js
@@ -10,6 +10,11 @@ require('cloud/createDriverAvailableRestaurants.js')
 require('cloud/createDriverAvailability.js')
 require('cloud/createRestaurant.js')
 require('cloud/changeDriverAvailabilityExpiryDate.js')
+require('cloud/fetchMySentRequests.js')
+require('cloud/getRequestsImPickingUp.js')
+require('cloud/deleteOrder.js')
+
+
 
 
 Parse.Cloud.define("hello", function(request, response) {
