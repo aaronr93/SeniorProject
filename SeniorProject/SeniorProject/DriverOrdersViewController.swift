bd8459e5793fe6231607117d7a95476c91ef048e
mkytkajr65 <mkytkajr@gmail.com>
2016-02-20 17:34:30 -0500

    order requests are now populated for both a specific driver as well as Any driver

--- SeniorProject/SeniorProject/DriverOrdersViewController.swift
+++ SeniorProject/SeniorProject/DriverOrdersViewController.swift
@@ -20,13 +20,22 @@ class DriverOrdersViewController: UITableViewController
     var sectionHeaders = ["Requests For Me","Requests For Anyone"]
     
     var driverOrders = [PFObject]()
+    var anyDriverOrders = [PFObject]()
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 2
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        switch section{
+        case 0:
+            return driverOrders.count
+        case 1:
+            return anyDriverOrders.count
+        default:
+            return 0
+        }
+        
     }
     
     override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -42,28 +51,71 @@ class DriverOrdersViewController: UITableViewController
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("order", forIndexPath: indexPath) as! OrderCell
-        
-        
+        var order : PFObject?
+        if indexPath.section == 0{
+            order = driverOrders[indexPath.row]
+        }else if indexPath.section == 1{
+            order = anyDriverOrders[indexPath.row]
+        }
+    
+        cell.restaurant?.text = order!["restaurant"]["name"] as? String
+        cell.recipient?.text = order!["OrderingUser"]["username"] as? String
         return cell
     }
     
     override func viewDidLoad() {
-        PFCloud.callFunctionInBackground("getOrdersForDriver", withParameters: ["user": "xrZ4ryBR8I"])
-            {
-            (jsonOrders, error) in
+        //get orders sent to the driver
+        let ordersForDriverQuery = PFQuery(className:"Order")
+        ordersForDriverQuery.includeKey("restaurant")
+        ordersForDriverQuery.includeKey("OrderingUser")
+        ordersForDriverQuery.whereKey("driverToDeliver", equalTo: PFUser.currentUser()!)
+    
+        ordersForDriverQuery.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?) -> Void in
+            
             if error == nil {
-                for order in (jsonOrders as? [PFObject])!{
-                    /*let thisOrderObj = Order()
-                    let orderingUser = order.objectForKey("OrderingUser") as! PFUser
-                    let restaurant = order.objectForKey("restaurant") as! PFObject
-                    let restaurantName = restaurant.objectForKey("name") as! String
-                    thisOrderObj.restaurant?.name = restaurantName
-                    thisOrderObj.OrderingUser = orderingUser*/
-                    self.driverOrders += [order]
+                // The find succeeded.
+                // Do something with the found objects
+                if let orders = objects {
+                    for order in orders {
+                        self.driverOrders.append(order)
+                    }
+                    self.tableView.reloadData()
                 }
-                print(self.driverOrders)
-                
+            } else {
+                // Log details of the failure
+                print("Error: \(error!) \(error!.userInfo)")
             }
         }
+        
+        //get any driver available orders
+        
+        let ordersForAnyDriverQuery = PFQuery(className:"Order")
+        ordersForAnyDriverQuery.includeKey("restaurant")
+        ordersForAnyDriverQuery.includeKey("OrderingUser")
+        ordersForAnyDriverQuery.whereKey("isAnyDriver", equalTo: true)
+        
+        ordersForAnyDriverQuery.findObjectsInBackgroundWithBlock {
+            (objects: [PFObject]?, error: NSError?) -> Void in
+            
+            if error == nil {
+                // The find succeeded.
+                // Do something with the found objects
+                if let orders = objects {
+                    for order in orders {
+                        if (order["driverToDeliver"] == nil){
+                            self.anyDriverOrders.append(order)
+                        }
+                    }
+                    print(self.anyDriverOrders)
+                    self.tableView.reloadData()
+                }
+            } else {
+                // Log details of the failure
+                print("Error: \(error!) \(error!.userInfo)")
+            }
+        }
+        
+        
     }
 }
