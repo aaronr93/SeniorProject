cce3bc88d0d0821cac43136c9c0e4383ef6bf6cd
Aaron Rosenberger <RosenbergerGA1@gcc.edu>
2016-02-22 17:49:01 -0500

    added UI tests for New Order

---
+++ SeniorProject/SeniorProjectUITests/NewOrderUITests.swift
@@ -0,0 +1,40 @@
+//
+//  NewOrderUITests.swift
+//  SeniorProject
+//
+//  Created by Aaron Rosenberger on 2/22/16.
+//  Copyright © 2016 Gooey. All rights reserved.
+//
+
+import XCTest
+
+class NewOrderUITests: SeniorProjectUITests {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func restaurantTap() {
+
+    }
+    
+    func restaurantDrag() {
+        // Unsupported - should do nothing
+    }
+    
+    func addFoodItemTap() {
+        // Should segue to new screen
+    }
+    
+    func addFoodItemDrag() {
+        // Unsupported - should do nothing
+    }
+
+}
