e22233e5affc44f0748878b39af567a83435674e
salablang <salablang@gmail.com>
2016-03-06 02:26:18 -0500

    Please be more careful merging, people! -re-fixed bug that was overwritten by merge commit 5c57a99804e7 -fixed latent bug of enumerating row values to enumerated SECTION values in two places -re-added some comments -re-added switch statements that had been reverted to if/else/else statements by merge

--- SeniorProject/SeniorProject/MyOrderTableViewController.swift
+++ SeniorProject/SeniorProject/MyOrderTableViewController.swift
@@ -26,11 +26,11 @@ class MyOrderTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section{
-        case Section.Restaurant.rawValue:
+        case Section.Restaurant.rawValue: //single restaurant allowed (or 'select a restaurant' if none yet)
             return 1
-        case Section.Food.rawValue:
+        case Section.Food.rawValue: //2nd section lists food items
             return order.foodItems.count
-        case Section.Settings.rawValue:
+        case Section.Settings.rawValue: //3 rows in 3rd section -- 'delivered by', 'location', and 'expires in'
             return deliverySectionTitles.count
         default:
             return 0
@@ -54,14 +54,14 @@ class MyOrderTableViewController: UITableViewController {
     
     //display custom row data based on the section in question--restaurant, food, or delivery
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        if indexPath.section == Section.Restaurant.rawValue {
+        switch indexPath.section {
+        case Section.Restaurant.rawValue: //list restaurant choice (only 1 row)
             return cellForRestaurantSection(tableView, cellForRowAtIndexPath: indexPath)
-        } else if indexPath.section == Section.Food.rawValue {
+        case Section.Food.rawValue: //list food item in respective row
             return cellForFoodSection(tableView, cellForRowAtIndexPath: indexPath)
-        } else if indexPath.section == Section.Settings.rawValue {
+        case Section.Settings.rawValue: //list one of the three 'delivery' rows ('delivered by', 'location', or 'expires in')
             return cellForDeliverySection(tableView, cellForRowAtIndexPath: indexPath)
-        }
-        else {//shouldn't get here
+        default: //shouldn't get here
             let cell: UITableViewCell! = nil
             return cell
         }
@@ -93,11 +93,11 @@ class MyOrderTableViewController: UITableViewController {
     func fillValuesBasedOn(row: Int) -> String {
         var value : String = ""
         switch row {
-        case Section.Restaurant.rawValue:
-            value = order.deliverTo
-        case Section.Food.rawValue:
+        case 0: //list deliverer
+            value = order.deliveredBy
+        case 1: //delivery location
             value = order.location
-        case Section.Settings.rawValue:
+        case 2: //time unti order expiration
             value = order.expiresIn
         default: //shouldn't get here
             value = ""
