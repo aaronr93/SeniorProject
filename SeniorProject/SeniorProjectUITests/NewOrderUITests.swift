f785b3262d8e2fca6c84556d12fce6f8199162a7
Zach Nafziger <zachary@nafziger.net>
2016-04-06 20:11:03 -0400

    updated new order ui tests - including a combinatoric test that checks every possible combination of filled out vs. non filled out items

--- SeniorProject/SeniorProjectUITests/NewOrderUITests.swift
+++ SeniorProject/SeniorProjectUITests/NewOrderUITests.swift
@@ -24,83 +24,85 @@ class NewOrderUITests: SeniorProjectUITests {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     
-    func testRestaurantTap() {
+    func testSelectRestaurant() {
         let app = XCUIApplication()
         app.buttons["I want food"].tap()
         
+        let tablesQuery = app.tables
+        let selectRestaurantStaticText = tablesQuery.staticTexts["Select restaurant..."]
         //not selecting a restaurant - none selected
-        XCTAssertNotNil(app.tables.staticTexts["Select restaurant..."])
-        app.tables.staticTexts["Select restaurant..."].tap()
-        XCTAssertNotNil(app.navigationBars["Select Restaurant"].buttons["New Order"])
-        app.navigationBars["Select Restaurant"].buttons["New Order"].tap()
-        XCTAssertNotNil(app.tables.staticTexts["Select restaurant..."])
-        
+        selectRestaurantStaticText.tap()
+        let newOrderButton = app.navigationBars["Select Restaurant"].buttons["New Order"]
+        newOrderButton.tap()
+        XCTAssertNotNil(selectRestaurantStaticText) //if text still exists, a restaurant hasn't been selected
         //selecting a restaurant
-        app.tables.staticTexts["Select restaurant..."].tap()
-        app.tables.staticTexts["sheetz"].tap()
-        XCTAssertNotNil(app.tables.staticTexts["Sheetz"])
-        
+        selectRestaurantStaticText.tap()
+        let northCountryBrewingCompanyStaticText = tablesQuery.staticTexts["North Country Brewing Company"]
+        northCountryBrewingCompanyStaticText.tap()
+        XCTAssertNotNil(northCountryBrewingCompanyStaticText)
+        northCountryBrewingCompanyStaticText.tap()
+        XCTAssertNotNil(northCountryBrewingCompanyStaticText)
+        newOrderButton.tap()
         //not selecting a restaurant - already something selected
-        app.tables.staticTexts["Sheetz"].tap()
-        app.navigationBars["Select Restaurant"].buttons["New Order"].tap()
-        XCTAssertNotNil(app.tables.staticTexts["Sheetz"])
         app.navigationBars["New Order"].buttons["Cancel"].tap()
-        
     }
     
     func testAddFoodItem(){
         
         let app = XCUIApplication()
-        //waitForExpectationsWithTimeout(10, handler: nil)
         app.buttons["I want food"].tap()
+        
         let tablesQuery = app.tables
-        tablesQuery.otherElements["FOOD"].tap()
-        
-        let clearTextTextField = tablesQuery.textFields.containingType(.Button, identifier:"Clear text").element
-        clearTextTextField.typeText("Test")
-        
-        app.buttons["Next"].tap()
-        app.tables.textFields["Details"].typeText("\n")
-        clearTextTextField.typeText("Test")
-        app.keyboards.buttons["Done"].tap()
-        app.typeText("\n")
-        
-        
-        
-        /*app.tables.otherElements.containingType(.StaticText, identifier:"Food items").childrenMatchingType(.Button).element.tap()
-        app.textFields["Enter Food Name"].typeText("test")
-        app.buttons["Next"].tap()
-        let enterFoodDescriptionTextField = app.textFields["Enter Food Description"]
-        enterFoodDescriptionTextField.tap()
-        enterFoodDescriptionTextField.typeText("test")
-        app.keyboards.buttons["Done"].tap()*/
-        
-        //should take us to edit food item screen, which means the food item was created
-        app.tables.element.cells.elementBoundByIndex(1).tap()
-        XCTAssertNotNil(clearTextTextField)
-        app.navigationBars["Food item"].buttons["New Order"].tap()
-        
+        tablesQuery.staticTexts["Add new food item..."].tap()
+        XCTAssertNotNil(tablesQuery.textFields["Title"])
+        tablesQuery.textFields["Title"].tap()
+        tablesQuery.textFields["Title"].typeText("test")
+        app.buttons["Next:"].tap()
+        tablesQuery.textFields["Details"].tap()
+        tablesQuery.textFields["Details"].typeText("test")
+        tablesQuery.textFields.containingType(.Button, identifier:"Clear text").element
+        app.navigationBars["Food item"].buttons["Done"].tap()
+        XCTAssertNotNil(app.navigationBars["New Order"].buttons["Cancel"])
+                        
+    }
+    
+    func testAddFoodItemII(){
+        testAddFoodItem()
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["Add new food item..."].tap()
+        XCTAssertNotNil(tablesQuery.textFields["Title"])
+        tablesQuery.textFields["Title"].tap()
+        tablesQuery.textFields["Title"].typeText("test")
+        app.buttons["Next:"].tap()
+        tablesQuery.textFields["Details"].tap()
+        tablesQuery.textFields["Details"].typeText("test")
+        tablesQuery.textFields.containingType(.Button, identifier:"Clear text").element
+        app.navigationBars["Food item"].buttons["Done"].tap()
+        XCTAssertNotNil(app.navigationBars["New Order"].buttons["Cancel"])
+        app.navigationBars["New Order"].buttons["Cancel"].tap()
     }
     
     
     func testSelectDriver(){
+        
         let app = XCUIApplication()
         app.buttons["I want food"].tap()
-        
-        //need to select a restaurant first
+        //selecting nothing
         let tablesQuery = app.tables
-        tablesQuery.staticTexts["Select restaurant..."].tap()
-        tablesQuery.staticTexts["sheetz"].tap()
-        
-        //can't test the values in cells, so basically just making sure it doesn't crash and goes to correct view
-        let deliveredByStaticText = tablesQuery.staticTexts["Delivered by"]
-        deliveredByStaticText.tap()
-        tablesQuery.staticTexts["testAccount"].tap()
-        XCTAssertNotNil(app.navigationBars["SeniorProject.ChooseDriverTableView"].buttons["New Order"])
-        deliveredByStaticText.tap()
-        XCTAssertNotNil(app.navigationBars["SeniorProject.ChooseDriverTableView"].buttons["New Order"])
+        app.tables.staticTexts["Select a driver..."].tap()
+        app.navigationBars["SeniorProject.ChooseDriverTableView"].buttons["New Order"].tap()
+        XCTAssertNotNil(tablesQuery.staticTexts["Select a driver..."])
+        //selecting any driver - the only consistent value on the menu
+        tablesQuery.staticTexts["Select a driver..."].tap()
         tablesQuery.staticTexts["Any driver"].tap()
+        XCTAssertNotNil(tablesQuery.staticTexts["Select a driver..."]) //should automatically be taken back
         app.navigationBars["New Order"].buttons["Cancel"].tap()
+        
+        
+        
+        
+        
     }
     
     func testSelectLocation(){
@@ -108,16 +110,19 @@ class NewOrderUITests: SeniorProjectUITests {
         app.buttons["I want food"].tap()
         let tablesQuery2 = app.tables
         let tablesQuery = tablesQuery2
-        tablesQuery.staticTexts["Location"].tap()
+        app.tables.staticTexts["Select delivery location..."].tap()
         tablesQuery.textFields["Custom delivery location..."].tap()
         tablesQuery2.cells.containingType(.Button, identifier:"Add").childrenMatchingType(.TextField).element.typeText("test")
         tablesQuery.buttons["Add"].tap()
         app.navigationBars["Select Delivery Location"].buttons["New Order"].tap()
-        tablesQuery.staticTexts["Location"].tap()
+        app.tables.staticTexts["Select delivery location..."].tap()
         //new table item should exist, tapping it should not break
         XCTAssertNotNil(app.tables.element.cells.elementBoundByIndex(1))
         app.tables.element.cells.elementBoundByIndex(1).tap()
         app.navigationBars["New Order"].buttons["Cancel"].tap()
+        
+        
+        
     
         
     }
@@ -125,27 +130,103 @@ class NewOrderUITests: SeniorProjectUITests {
     func testSelectExpiration(){
         
         let app = XCUIApplication()
-        let cancelButton = app.navigationBars["New Order"].buttons["Cancel"]
         app.buttons["I want food"].tap()
         
+        let tablesQuery = app.tables
+        tablesQuery.otherElements["FOOD"].tap()
+        
         let app2 = app
-        app2.tables.staticTexts["Expires In"].tap()
-        app2.pickerWheels.element.adjustToPickerWheelValue("2 Hours")
-        XCTAssertNotNil(app.navigationBars["Expires In"].buttons["New Order"])
+        app2.tables.staticTexts["Select expiration time..."].tap()
+        app2.pickerWheels.elementBoundByIndex(0).adjustToPickerWheelValue("2 hours")
+        app2.pickerWheels.elementBoundByIndex(1).adjustToPickerWheelValue("15 minutes")
         let newOrderButton = app.navigationBars["Expires In"].buttons["New Order"]
+        XCTAssertNotNil(newOrderButton)
         newOrderButton.tap()
-        cancelButton.tap()
-    }
-    
-    func testBackButtononSelectExpiration() {
+        XCTAssertNotNil(tablesQuery.cells.containingType(.StaticText, identifier:"Expires in").childrenMatchingType(.StaticText).matchingIdentifier("Expires in").elementBoundByIndex(0))
+        tablesQuery.cells.containingType(.StaticText, identifier:"Expires in").childrenMatchingType(.StaticText).matchingIdentifier("Expires in").elementBoundByIndex(0).tap()
+        newOrderButton.tap()
+        app.navigationBars["New Order"].buttons["Cancel"].tap()
         
-        
     }
     
     
-    func testAddFoodItemDrag() {
-        // Unsupported - should do nothing
+    
+   
+    
+    func testRequestCombinatoric(){
+        //this tests every possible combination
+        let onOff = [true, false]
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        let selectRestaurantStaticText = tablesQuery.staticTexts["Select restaurant..."]
+        for restaurant in onOff{
+            for food in onOff{
+                for driver in onOff{
+                    for location in onOff{
+                        for expiration in onOff{
+                            //go to the new order
+                            app.buttons["I want food"].tap()
+                            
+                            if restaurant{
+                                selectRestaurantStaticText.tap()
+                                tablesQuery.staticTexts.elementBoundByIndex(0).tap()
+                            }
+                            
+                            if food{
+                                tablesQuery.staticTexts["Add new food item..."].tap()
+                                XCTAssertNotNil(tablesQuery.textFields["Title"])
+                                tablesQuery.textFields["Title"].tap()
+                                tablesQuery.textFields["Title"].typeText("test")
+                                app.buttons["Next:"].tap()
+                                tablesQuery.textFields["Details"].tap()
+                                tablesQuery.textFields["Details"].typeText("test")
+                                tablesQuery.textFields.containingType(.Button, identifier:"Clear text").element
+                                app.navigationBars["Food item"].buttons["Done"].tap()
+                            }
+                            
+                            if driver{
+                                tablesQuery.staticTexts["Select a driver..."].tap()
+                                tablesQuery.staticTexts["Any driver"].tap()
+                            }
+                            
+                            if location{
+                                tablesQuery.staticTexts["Select delivery location..."].tap()
+                                tablesQuery.textFields["Custom delivery location..."].tap()
+                                tablesQuery.cells.containingType(.Button, identifier:"Add").childrenMatchingType(.TextField).element.typeText("test")
+                                tablesQuery.buttons["Add"].tap()
+                                XCTAssertNotNil(app.tables.element.cells.elementBoundByIndex(1))
+                                app.tables.element.cells.elementBoundByIndex(1).tap()
+                            }
+                            
+                            if expiration{
+                                tablesQuery.staticTexts["Select expiration time..."].tap()
+                                app.pickerWheels.elementBoundByIndex(0).adjustToPickerWheelValue("2 hours")
+                                app.pickerWheels.elementBoundByIndex(1).adjustToPickerWheelValue("15 minutes")
+                                app.navigationBars["Expires In"].buttons["New Order"].tap()
+                            }
+                            
+                            tablesQuery.buttons["Submit"].tap()
+                            
+                            
+                            if(restaurant && food && driver && location && expiration){
+                                //if everything is filled out, make sure we're home
+                                XCTAssertNotNil(app.navigationBars["Home"].buttons["My orders"])
+                                app.navigationBars["Home"].buttons["My orders"].tap()
+                                app.navigationBars["My Orders"].buttons["Home"].tap()
+                            }
+                            else{
+                                // if not, check we're still on the new order page
+                                XCTAssertNotNil(app.navigationBars["New Order"].buttons["Cancel"])
+                                app.navigationBars["New Order"].buttons["Cancel"].tap()
+                            }
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
+    
     
     func testCancelNewOrder() {
         let app = XCUIApplication()
